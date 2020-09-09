fb56b26649193e74060a979b8fd9fdc19c9dea8b
Nicholas Schamberg <schamberg.nicholas@gmail.com>
2020-09-10 00:26:09 +0300

    Typo fix

--- test/app.listen.js
+++ test/app.listen.js
@@ -4,7 +4,7 @@ var express = require('../')
 describe('app.listen()', function () {
   it('should wrap with an HTTP server', function (done) {
     if (process.env.UWS_SERVER_ENABLED_FOR_TEST === 'TRUE') {
-      console.log('\x1b[31m%s\x1b[0m', 'TFyrejet possibly doesn\'t conform to Express API here')
+      console.log('\x1b[31m%s\x1b[0m', 'Fyrejet possibly doesn\'t conform to Express API here')
       done()
     }
     var app = express()
