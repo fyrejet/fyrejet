d8e1d9188414c45d6b801dea2a8e8c8c25b342c8
Nicholas Schamberg <schamberg.nicholas@gmail.com>
2021-02-27 19:03:51 +0300

    test changes

--- test/res.send.js
+++ test/res.send.js
@@ -50,63 +50,33 @@ describe('res', function () {
     })
   })
 
-  describe('.send(code)', function () {
-    it('should set .statusCode', function (done) {
-      var app = express()
-
-      app.use(function (req, res) {
-        res.send(201)
-      })
-
-      request(app)
-        .get('/')
-        .expect('Created')
-        .expect(201, done)
-    })
-  })
-
-  describe('.send(code, body)', function () {
-    it('should set .statusCode and body', function (done) {
-      var app = express()
-
-      app.use(function (req, res) {
-        res.send(201, 'Created :)')
-      })
-
-      request(app)
-        .get('/')
-        .expect('Created :)')
-        .expect(201, done)
-    })
-  })
-
-  describe('.send(body, code)', function () {
-    it('should be supported for backwards compat', function (done) {
-      var app = express()
-
-      app.use(function (req, res) {
-        res.send('Bad!', 400)
-      })
-
-      request(app)
-        .get('/')
-        .expect('Bad!')
-        .expect(400, done)
-    })
-  })
-
-  describe('.send(code, number)', function () {
+  describe('.send(number)', function () {
     it('should send number as json', function (done) {
       var app = express()
 
       app.use(function (req, res) {
-        res.send(200, 0.123)
+        res.status(200).send(0.123)
       })
 
       request(app)
         .get('/')
         .expect('Content-Type', 'application/json; charset=utf-8')
         .expect(200, '0.123', done)
+    })
+  })
+
+  describe('.send(number, string, object)', function () {
+    it('should send number as json', function (done) {
+      var app = express()
+
+      app.use(function (req, res) {
+        res.status(200).send(0.123, 'foo', {foo: "bar"})
+      })
+
+      request(app)
+        .get('/')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, '[0.123,"foo",{"foo":"bar"}]', done)
     })
   })
 
